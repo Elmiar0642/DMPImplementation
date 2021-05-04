6ae1fcae8785fc3e0af77619fa2ce0c91ea191d8
ARIMA VALAN IMMANUEL <35724936+ArimaValanImmanuel@users.noreply.github.com>
2021-05-04 18:14:55 +0530

    Changed

--- weightgeneration.py
+++ weightgeneration.py
@@ -14,71 +14,158 @@ print("STARTED AT : ", start_time)
 
 P = []*0
 
-DIR = "A:\\Arima\\PROJECTS\\Outbox\\Test5\\a\\train_data.h5"
+DIR = "/content/data.h5"
 
-WEIGHT = "weight_data.h5"
+#WEIGHT = "/content/trainedweigtsforbees.h5" 
 
-files = h5py.File(DIR, 'r+')
+files = h5py.File(DIR, 'r')
 
-wt_file = h5py.File(WEIGHT, 'w')
+#weight = h5py.File(WEIGHT, 'w')
+
+predcat, predsubcat = "c2", "031_002"
+
+#print(files["c2"]["031_002"][:, :, :-1].nbytes)
+
+predimg = np.array([1])
+
+predimg = np.append(predimg, files["c2"]["031_002"][100:201, 100:200, :-1].flatten())
+
+#print(predimg.nbytes)
+
+#input()
 
 labels = []*0
 
+x_rgb, y_label = []*0, []*0
+
 for category in files.keys():
 
-    for subcategory in files[category].keys():
+    for subcategory in files[category].keys():         
 
         if subcategory[:3] not in labels:
 
             labels.append(subcategory[:3])
 
-        replacementval = labels.index(subcategory[:3])+1
+        #replacementval = labels.index(subcategory[:3])+1
 
-        files[category][subcategory][:, :, -1] = replacementval
+        #files[category][subcategory][:, :, -1] = replacementval
 
-        shape = files[category][subcategory][:, :, :].shape
+        #shape = files[category][subcategory][:, :, :].shape
+        #for j in range(shape[1]):
 
-        for j in range(shape[1]):
+            #files[category][subcategory][:, j, :] = np.roll(files[category][subcategory][:, j, :], 1)
 
-            files[category][subcategory][:, j, :] = np.roll(files[category][subcategory][:, j, :], 1)
+        #img = files[category][subcategory][:, :, :]
 
-        img = files[category][subcategory][:, :, :]
+        #P.append(img)
 
-        P.append(img)
+        dmpimg = np.array([1])
 
-P = np.array(P)
+        dmpimg = np.append(dmpimg, files[category][subcategory][100:201, 100:200, :-1].flatten())
+        
+        x_rgb.append(dmpimg)
+        
+        y_label.append(files[category][subcategory][100:201, 100:200, -1][-1][-1])
 
-print(P.shape)
+        #break
 
-P = P.transpose(1, 2, 3, 0)
+    
+#P = np.array(P)
+x_rgb = np.array(x_rgb, dtype = np.uint8)
 
-print(P.shape)
+#print(x_rgb.shape)
 
-PT = P.transpose(1, 0, 2, 3)
+y_label = np.array(y_label)
 
-print(P.shape, PT.shape)
+#print(x_rgb, len(x_rgb), y_label, len(y_label))
 
-PIPT = P * PT
+c = x_rgb
 
-I = np.random.normal(0,1,(PIPT.shape[0], PIPT.shape[1], PIPT.shape[2], PIPT.shape[3]))
+#c = c.T
 
-print(PIPT.shape, I.shape)
+print(c.nbytes)
 
-PIPTPI = PIPT + I
+print(c.shape)
 
-print(PIPTPI.shape)
+#input()
 
-PIPTPII = np.linalg.inv(PIPTPI.T)
+#input()
 
-print(PIPTPII.shape)
+ct = c.T
 
-alpha = PIPTPII.T * PT
+#print(ct)
+
+print(ct.shape)
+
+cf = np.matmul(c, ct)
+
+#cf = cf.astype('float32')
+
+#input()
+
+#cf = cf.flatten()
+
+#print(cf)
+
+print(cf.shape)
+
+trdata = "/content/trdata.h5"
+
+traininghalf = h5py.File(trdata, 'w')
+
+traininghalf.create_dataset("before_inversion", data = cf)
+
+traininghalf.close()
+
+import tensorflow as tf
+
+import numpy as np
+
+import h5py
+
+traininghalf = h5py.File("/content/trdata.h5", 'r')
+
+cf = traininghalf["before_inversion"][:, :]
+
+print(cf.shape)
+
+cf = cf.astype('float64')
+
+#rhs = np.ones((cf.shape[0], cf.shape[1]))
+
+ci = np.linalg.inv(cf)
+
+#print(ci)
+
+print(ci.shape)
+
+#ci = np.abs(ci)
+
+#print(ci)
+
+#print(ci.shape)
+
+alp = np.matmul(ci, c)
+
+alp = np.abs(alp)
+
+#print(alp)
+
+print(alp.shape)
+
+alpha = alp * y_label[:, None]
+
+#print(alpha)
 
 print(alpha.shape)
 
-wt_file.create_dataset("weight_alpha", data = alpha)
+WEIGHT = "/content/trainedweigtsforbees.h5" 
 
-wt_file.close()
+weight = h5py.File(WEIGHT, "w")
+
+weight.create_dataset("weight_alpha", data = alpha)
+
+weight.close()
 
 end_time = datetime.now()
 
